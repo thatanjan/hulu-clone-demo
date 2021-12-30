1928f150e41836db577baf9bd3b8651e916c53b9
thatanjan <anjan.cules@gmail.com>
2021-12-30 16:30:46 +0600

    login modal is done

--- js/index.js
+++ js/index.js
@@ -2,10 +2,20 @@ const footerSectionEls = document.querySelectorAll('.footer__section')
 const sectionExpandIconEl = document.querySelector('.expand__section__icon')
 const footerTitleEls = document.querySelectorAll('.footer__section__title')
 
+const loginLinkEl = document.querySelector('.header .login__link')
+const modalContainerEl = document.querySelector('.modal__container')
+const modalCloseEl = document.querySelector('.modal__container .close__icon')
+
 const is_expanded = 'is-expanded'
+const is_open = 'is-open'
 
 const toggleExpandSection = element => () =>
 	element.classList.toggle(is_expanded)
+
+const toggleModal = () => modalContainerEl.classList.toggle(is_open)
+
+loginLinkEl.addEventListener('click', toggleModal)
+modalCloseEl.addEventListener('click', toggleModal)
 
 footerTitleEls.forEach(el =>
 	el.addEventListener('click', toggleExpandSection(el.parentElement))
